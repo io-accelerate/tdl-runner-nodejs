d1df70da09a024cb0e76b4d346ba3a91741f86c5
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2025-11-18 16:34:55 +0000

    feat: compute coverage

--- test/solutions/SUM/test-sum-solution.js
+++ test/solutions/SUM/test-sum-solution.js
@@ -1,4 +1,4 @@
-import { describe, it } from 'mocha';
+import { describe, it } from 'node:test';
 import assert from 'node:assert';
 import SumSolution from '../../../lib/solutions/SUM/sum_solution.js';
 
